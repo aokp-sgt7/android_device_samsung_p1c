8b3b711bdd15c171cd64ed89c13484cc8fb90e69
stimpz0r <stimpz0r@kriticalcrew.org>
2012-06-26 23:50:53 +1000

    BUILD #40 - includes ascii logo / art by myself

--- aokp.mk
+++ aokp.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/aokp/configs/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := P1C
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/p1c/full_p1c.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p1c
-PRODUCT_NAME := cm_p1c
+PRODUCT_NAME := full_p1c
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-I800
 
